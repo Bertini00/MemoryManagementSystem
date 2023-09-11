534d4daf9057eb800230642d338869c812695e43
Bertini00 <bertini.luca00@gmail.com>
2023-09-11 21:40:37 +0200

    added some comments to chunk deallocate function

--- MemoryManagement/Chunk.cpp
+++ MemoryManagement/Chunk.cpp
@@ -26,14 +26,17 @@ void* Chunk::Allocate(size_t blockSize) {
 }
 
 void Chunk::Deallocate(void* p, size_t blockSize) {
+	// Cast to unsigned char so we can write on the first byte
 	unsigned char *deallocatedPointer = (unsigned char*)p;
-
+	
+	// Calculate the position of the soon to be first available block
 	unsigned char fab = (deallocatedPointer - pData_) / blockSize;
 
+	// Write the previoud available block into the block we are freeing
 	*deallocatedPointer = firstAvailableBlock_;
 
+	// Swap the first available block with the one we are freeing
 	firstAvailableBlock_ = fab;
-
 
 	return;
 }
